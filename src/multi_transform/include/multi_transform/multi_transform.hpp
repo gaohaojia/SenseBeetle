3e7afe35dd8398398493e37bc9a0b49846f32c52
gaohaojia <2723687563@qq.com>
2024-05-14 13:05:38 +0800

    update multi transform

--- src/multi_transform/include/multi_transform/multi_transform.hpp
+++ src/multi_transform/include/multi_transform/multi_transform.hpp
@@ -1,11 +1,27 @@
 #ifndef MULTI_TRANSFORM
 #define MULTI_TRANSFORM
 
+#include <memory>
+
 #include <rclcpp/node.hpp>
 #include <rclcpp/node_options.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <pcl/impl/point_types.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include "tf2/transform_datatypes.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_eigen/tf2_eigen.hpp"
+#include "tf2_ros/transform_listener.h"
+
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 namespace multi_transform
 {
@@ -17,14 +33,24 @@ public:
 private:
   int robot_id;
 
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::shared_ptr<Eigen::Matrix4d> formIdMapToMap;
+
   void TerrainMapCallBack(const sensor_msgs::msg::PointCloud2::ConstSharedPtr terrain_map_msg);
   void TerrainMapExtCallBack(const sensor_msgs::msg::PointCloud2::ConstSharedPtr terrain_map_ext_msg);
+  void RegisteredScanCallBack(const sensor_msgs::msg::PointCloud2::ConstSharedPtr registered_scan_msg);
+  void StateEstimationAtScanCallBack(const nav_msgs::msg::Odometry::ConstSharedPtr state_estimation_at_scan_msg);
 
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr terrain_map_sub_;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr terrain_map_ext_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr registered_scan_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr state_estimation_at_scan_sub_;
 
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr total_terrain_map_pub_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr total_terrain_map_ext_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr total_registered_scan_pub_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr total_state_estimation_at_scan_pub_;
 };
 }  // namespace multi_transform
 
--- src/multi_transform/src/multi_transform.cpp
+++ src/multi_transform/src/multi_transform.cpp
@@ -1,5 +1,12 @@
 #include <functional>
-#include <rclcpp/node_options.hpp>
+#include <memory>
+#include <nav_msgs/msg/detail/odometry__struct.hpp>
+#include <pcl/common/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/msg/detail/point_cloud2__struct.hpp>
+#include <string>
+
+#include "geometry_msgs/msg/transform_stamped.hpp"
 
 #include "multi_transform/multi_transform.hpp"
 
@@ -11,17 +18,93 @@ MultiTransformNode::MultiTransformNode(const rclcpp::NodeOptions& options)
   this->declare_parameter<int>("robot_id", 0);
   this->get_parameter("robot_id", robot_id);
 
-  terrain_map_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("terrain_map_", 2, std::bind(&MultiTransformNode::TerrainMapCallBack, this, std::placeholders::_1));
-  terrain_map_ext_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("terrain_map_ext_", 2, std::bind(&MultiTransformNode::TerrainMapExtCallBack, this, std::placeholders::_1));
-  total_terrain_map_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/total_terrain_map", 2);
-  total_terrain_map_ext_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/total_terrain_map_ext", 2);
+  terrain_map_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "terrain_map",
+    2,
+    std::bind(&MultiTransformNode::TerrainMapCallBack, this, std::placeholders::_1));
+  terrain_map_ext_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "terrain_map_ext",
+    2,
+    std::bind(&MultiTransformNode::TerrainMapExtCallBack, this, std::placeholders::_1));
+  registered_scan_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "registered_scan",
+    5,
+    std::bind(&MultiTransformNode::RegisteredScanCallBack, this, std::placeholders::_1));
+  state_estimation_at_scan_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    "state_estimation_at_scan",
+    5,
+    std::bind(&MultiTransformNode::StateEstimationAtScanCallBack, this, std::placeholders::_1));
+
+  total_terrain_map_pub_ =
+    this->create_publisher<sensor_msgs::msg::PointCloud2>("/total_terrain_map", 2);
+  total_terrain_map_ext_pub_ =
+    this->create_publisher<sensor_msgs::msg::PointCloud2>("/total_terrain_map_ext", 2);
+  total_registered_scan_pub_ =
+    this->create_publisher<sensor_msgs::msg::PointCloud2>("total_registered_scan", 2);
+  total_state_estimation_at_scan_pub_ =
+    this->create_publisher<nav_msgs::msg::Odometry>("total_state_estimation_at_scan", 2);
+
+  std::shared_ptr<geometry_msgs::msg::TransformStamped> transformStamped;
+  std::string fromFrameRel = "robot_" + std::to_string(robot_id) + "/map";
+  std::string toFrameRel = "map";
+  tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+  try {
+    transformStamped = std::make_shared<geometry_msgs::msg::TransformStamped>(
+      tf_buffer_->lookupTransform(toFrameRel, fromFrameRel, tf2::TimePointZero));
+  } catch (const tf2::TransformException& ex) {
+    RCLCPP_INFO(this->get_logger(),
+                "Could not transform %s to %s: %s",
+                toFrameRel.c_str(),
+                fromFrameRel.c_str(),
+                ex.what());
+    return;
+  }
+  formIdMapToMap = std::make_shared<Eigen::Matrix4d>(
+    tf2::transformToEigen(transformStamped->transform).matrix().cast<double>());
 }
 
-void MultiTransformNode::TerrainMapCallBack(const sensor_msgs::msg::PointCloud2::ConstSharedPtr terrain_map_msg){
-
+void MultiTransformNode::TerrainMapCallBack(
+  const sensor_msgs::msg::PointCloud2::ConstSharedPtr terrain_map_msg)
+{
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_tmp(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_result(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::fromROSMsg(*terrain_map_msg, *pointcloud_tmp);
+  pcl::transformPointCloud(*pointcloud_tmp, *pointcloud_result, *formIdMapToMap);
+  std::shared_ptr<sensor_msgs::msg::PointCloud2> totalTerrainCloud;
+  pcl::toROSMsg(*pointcloud_result, *totalTerrainCloud);
+  totalTerrainCloud->header.stamp = terrain_map_msg->header.stamp;
+  totalTerrainCloud->header.frame_id = "map";
+  total_terrain_map_pub_->publish(*totalTerrainCloud);
 }
 
-void MultiTransformNode::TerrainMapExtCallBack(const sensor_msgs::msg::PointCloud2::ConstSharedPtr terrain_map_ext_msg){
+void MultiTransformNode::TerrainMapExtCallBack(
+  const sensor_msgs::msg::PointCloud2::ConstSharedPtr terrain_map_ext_msg)
+{
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_tmp(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_result(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::fromROSMsg(*terrain_map_ext_msg, *pointcloud_tmp);
+  pcl::transformPointCloud(*pointcloud_tmp, *pointcloud_result, *formIdMapToMap);
+  std::shared_ptr<sensor_msgs::msg::PointCloud2> totalTerrainExtCloud;
+  pcl::toROSMsg(*pointcloud_result, *totalTerrainExtCloud);
+  totalTerrainExtCloud->header.stamp = terrain_map_ext_msg->header.stamp;
+  totalTerrainExtCloud->header.frame_id = "map";
+  total_terrain_map_ext_pub_->publish(*totalTerrainExtCloud);
+}
 
+void MultiTransformNode::RegisteredScanCallBack(const sensor_msgs::msg::PointCloud2::ConstSharedPtr registered_scan_msg){
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_tmp(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_result(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::fromROSMsg(*registered_scan_msg, *pointcloud_tmp);
+  pcl::transformPointCloud(*pointcloud_tmp, *pointcloud_result, *formIdMapToMap);
+  std::shared_ptr<sensor_msgs::msg::PointCloud2> registeredScanCloud;
+  pcl::toROSMsg(*pointcloud_result, *registeredScanCloud);
+  registeredScanCloud->header.stamp = registered_scan_msg->header.stamp;
+  registeredScanCloud->header.frame_id = "map";
+  total_registered_scan_pub_->publish(*registeredScanCloud);
+}
+
+void MultiTransformNode::StateEstimationAtScanCallBack(const nav_msgs::msg::Odometry::ConstSharedPtr state_estimation_at_scan_msg){
+  std::shared_ptr<nav_msgs::msg::Odometry> odom_temp = std::make_shared<nav_msgs::msg::Odometry>(new nav_msgs::msg::Odometry(*state_estimation_at_scan_msg));
 }
 } // namespace multi_transform
