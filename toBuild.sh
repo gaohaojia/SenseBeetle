f62e164482176b095a5b6c573837860333fe4a0f
Gao Haojia <2723687563@qq.com>
2024-09-28 18:02:18 +0800

    automatic compatibility architecture

--- toBuild.sh
+++ toBuild.sh
@@ -1,4 +1,1 @@
-cd src/tare_planner/or-tools/
-git checkout x86
-cd ../../..
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
--- toBuild_arm.sh
+++
@@ -1,4 +0,0 @@
-cd src/tare_planner/or-tools/
-git checkout arm
-cd ../../..
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
